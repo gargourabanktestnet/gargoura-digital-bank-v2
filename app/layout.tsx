e2685cf83420e7e797487563c9e8c0f2c386ba2b
gargourabanktestnet <gargouradigitalbank@gmail.com>
2026-09-18 16:18:24 +0100

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,3 @@
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return <html lang="fr"><head><script src="https://cdn.tailwindcss.com"></script></head><body style={{margin:0}}>{children}</body></html>
+}
