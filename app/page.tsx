616967e56992f1702f924ad99894b4e11071248a
gargourabanktestnet <gargouradigitalbank@gmail.com>
2026-09-19 23:02:48 +0100

    Update page title to 'GARGOURA DIGITAL BANK'

--- app/page.tsx
+++ app/page.tsx
@@ -1,2 +1,2 @@
 "use client";
-export default function Page(){return <div style={{background:"#0d3b8c",minHeight:"100vh",padding:"30px",color:"white",textAlign:"center"}}><h1>GARGOURA BANK</h1><div style={{fontSize:"50px",marginTop:"20px"}}>999 Pi BLEU</div><p>Si tu vois ça, c'est gagné!</p></div>}
+export default function Page(){return <div style={{background:"#0d3b8c",minHeight:"100vh",padding:"30px",color:"white",textAlign:"center"}}><h1>GARGOURA DIGITAL BANK</h1><div style={{fontSize:"50px",marginTop:"20px"}}>999 Pi BLEU</div><p>Si tu vois ça, c'est gagné!</p></div>}
