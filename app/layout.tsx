3351b8cecd136252416c308e18e30d565f444972
gargourabanktestnet <gargouradigitalbank@gmail.com>
2026-09-19 21:40:15 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,3 @@
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="fr">
-      <head>
-        <script src="https://cdn.tailwindcss.com"></script>
-        <script src="https://sdk.minepi.com/pi-sdk.js"></script>
-      </head>
-      <body style={{margin:0, fontFamily:'sans-serif'}}>{children}</body>
-    </html>
-  );
+  return <html lang="fr"><body style={{margin:0}}>{children}</body></html>
 }
