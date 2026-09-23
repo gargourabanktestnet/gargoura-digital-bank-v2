383718225ec6ae45da295f077ed4003188836846
gargourabanktestnet <gargouradigitalbank@gmail.com>
2026-09-23 18:56:21 +0100

    Delete app/terms directory

--- app/terms/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Terms(){
-  return <div style={{padding:"20px",fontFamily:"sans-serif",lineHeight:"1.6"}}>
-    <h1>Terms of Service - Gargoura Digital Bank</h1>
-    <p>This is a Testnet application for Pi Network.</p>
-    <p>1. No real money value in Testnet.<br/>2. User is responsible for correct wallet addresses.<br/>3. We provide P2P, Mobile Money simulation and bill payments.</p>
-    <p>By using the app, you agree to Pi Network Terms.</p>
-  </div>
-}
