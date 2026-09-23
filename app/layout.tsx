b437aeadbb95aeebdb061981eb214296ad927cfe
gargourabanktestnet <gargouradigitalbank@gmail.com>
2026-09-23 10:51:53 +0100

    Include Pi SDK script in RootLayout
    Added a script tag for the Pi SDK in the RootLayout component.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return <html lang="fr"><body style={{margin:0}}>{children}</body></html>
+import './globals.css'
+export default function RootLayout({children}:{children:React.ReactNode}){
+ return(<html lang="fr"><head><script src="https://sdk.minepi.com/pi-sdk.js"></script></head><body>{children}</body></html>)
 }
